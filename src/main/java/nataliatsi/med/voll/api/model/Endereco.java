1f6cc4085d203c2d85371248fb1045e954e4c681
Natália S. Gomes <nataliandsg32@gmail.com>
2024-06-07 19:08:01 -0300

    add: class medico & endereco

---
+++ src/main/java/nataliatsi/med/voll/api/model/Endereco.java
@@ -0,0 +1,30 @@
+package nataliatsi.med.voll.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String numero;
+    private String complemento;
+
+    public Endereco(EnderecoDTO endereco) {
+        this.logradouro = endereco.logradouro();
+        this.bairro = endereco.bairro();
+        this.cep = endereco.cep();
+        this.cidade = endereco.cidade();
+        this.uf = endereco.uf();
+        this.numero = endereco.numero();
+        this.complemento = endereco.complemento();
+    }
+}
