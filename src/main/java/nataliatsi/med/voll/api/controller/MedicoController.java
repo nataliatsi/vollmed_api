be44ccfa57c682f9600cd1651b1f793afc8bc5bb
Natália S. Gomes <nataliandsg32@gmail.com>
2024-06-07 19:08:23 -0300

    add: medico controller & repository

---
+++ src/main/java/nataliatsi/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,23 @@
+package nataliatsi.med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import nataliatsi.med.voll.api.model.Medico;
+import nataliatsi.med.voll.api.model.MedicoDTO;
+import nataliatsi.med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid MedicoDTO dados){
+        medicoRepository.save(new Medico(dados));
+    }
+}
---
+++ src/main/java/nataliatsi/med/voll/api/repository/MedicoRepository.java
@@ -0,0 +1,7 @@
+package nataliatsi.med.voll.api.repository;
+
+import nataliatsi.med.voll.api.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+}
