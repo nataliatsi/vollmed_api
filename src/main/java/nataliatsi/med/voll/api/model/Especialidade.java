3baf56ed52cca8e4264e8141a2358c1356ed5a5f
Natália S. Gomes <nataliandsg32@gmail.com>
2024-06-07 19:07:10 -0300

    add: dto file (medico & endereco)

---
+++ src/main/java/nataliatsi/med/voll/api/model/Especialidade.java
@@ -0,0 +1,28 @@
+package nataliatsi.med.voll.api.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Especialidade {
+    ORTOPEDIA ("ortopedia"),
+    CARDIOLOGIA ("cardiologia"),
+    GINECOLOGIA ("ginecologia"),
+    DERMATOLOGIA ("dermatologia");
+
+    private final String especialidade;
+
+    Especialidade(String especialidade){
+        this.especialidade = especialidade;
+    }
+
+    public static Especialidade fromString(String especialidade) {
+        for (Especialidade e : Especialidade.values()) {
+            if (e.especialidade.equalsIgnoreCase(especialidade)) {
+                return e;
+            }
+        }
+        throw new IllegalArgumentException("Especialidade não reconhecida: " + especialidade);
+    }
+
+}
+
---
+++ src/main/java/nataliatsi/med/voll/api/model/MedicoDTO.java
@@ -0,0 +1,24 @@
+package nataliatsi.med.voll.api.model;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record MedicoDTO(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        EnderecoDTO endereco
+) {
+}
